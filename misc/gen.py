35620a8aae668cfed83ecd57f40b083ab852aa99
SLAE-941 <slae_burner@protonmail.com>
2017-07-03 19:44:38 +0200

    Update gen.py

--- misc/gen.py
+++ misc/gen.py
@@ -1,37 +1,49 @@
-import random
-max_depth = 0
 
-def test_char(start, target, usable_chars):
+waterline = 0
+max_depth = 4
+
+usable_chars = [ '%', '_', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+                'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+                 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+                 'X', 'Y', 'Z', '-' ]
+
+def test_char(start, target):
+    global usable_chars
     for char in usable_chars:
-    #print(char)
-    #print((ord(char) + attempt) & 0x000000ff)
-    #print(char_list)
-        #print("Trying subchar %s" % char);
-        #print((ord(char) + start) & 0x000000ff)
         if ((ord(char) + start) & 0x000000ff) == ord(target):
-            #print(char)
             return char
     
     return False   
 
-def find_char(end_val, start_val , usable_chars, char_list, depth):
+def find_char(byte):
+    
+    global usable_chars
+    global waterline
     global max_depth
+
+    char_list = []
+
     success = False
     
     for char in usable_chars:
         #print("Trying %s" % char);
         test_val = ord(end_val) + ord(char)
         if test_val == start_val:
-            print("Early winner")
-            if (depth + 1) >= max_depth:
+            if checking:
+                
+            if (depth + 1) >= waterline:
                 depth += 1
+                if depth > max_depth:
+                    return False
                 success = True
                 char_list.append(char)
                 break
         
-        result = test_char(test_val, start_val, usable_chars)
+        result = test_char(test_val, start_val)
         if result is not False:
-            if (depth + 2) >= max_depth:
+            if (depth + 2) >= waterline:
                 success = True
                 depth += 2
                 print("Late winner")
@@ -41,83 +53,118 @@ def find_char(end_val, start_val , usable_chars, char_list, depth):
     
     if not success:
         print("Recursion!")
-        print(char_list)
         depth += 1
-        char_list.append(usable_chars[0])
-        test_val = ord(end_val) + ord(usable_chars[0])
-        if depth > max_depth:
-            max_depth = depth
-        return find_char(chr(test_val), start_val, usable_chars, char_list, depth)
+        for char in usable_chars:
+                temp_byte = byte
+                temp_byte['end_val'] = (ord(end_val) + ord(char)) & 0x000000ff
+                result = find_char(temp_byte)
+        if depth > waterline:
+            waterline = depth
+        return find_char(chr(test_val), start_val, char_list, depth)
 
-    if depth > max_depth:
+    if depth > waterline:
         print("Getting deeper")
-        max_depth = depth
+        waterline = depth
         
     return depth
 
-def check_depth(byte1, byte2, byte3, byte4):
-    if byte1[1] == max_depth and byte2[1] == max_depth and byte3[1] == max_depth and byte4[1] == max_depth:
-        return True
-    return False
+def check_depth(byte):
+    global waterline
+    balanced = True
 
-def get_carry(value_list):
-    sum = 0
-    for char in value_list:
+    if waterline == 0:
+        return False
+    
+    for i in range(0,len(byte)):
+        if byte[i]['depth'] != waterline:
+            balanced = False
+    return balanced
+
+def get_carry(byte):
+    sum = ord(byte['value'])
+    for char in byte['char_list']:
         sum += ord(char)
     return (sum & 0x0000ff00) >> 8
 
-def hex_print(byte1, byte2, byte3, byte4):
-    depth = byte1[1]
-    for i in range(0,depth):
-        print(hex(ord(byte1[2][i])))
+def hex_print(byte):
+    global waterline
+    for i in range(0,waterline):
+        value = ''
+        for byte_count in range(0,len(byte)):
+            value = hex(ord(byte[byte_count]['char_list'][i]))[2:] + value
+        print('0x' + value)
 
 if __name__ == "__main__":
-    
-    usable_chars = [ '%', '_', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-                     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-                     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
-                     'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
-                     'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
-                     'X', 'Y', 'Z', '-' ]
-    
-    byte1 = ['\x41', 0, []]
-    byte2 = ['\x41', 0, []]
-    byte3 = ['\x00', 0, []]
-    byte4 = ['\x41', 0, []]
 
-    random.seed()
+    # The intention is to have a start value and an end value e.g: the start value is 0x00000000
+    # and an end value e.g. 0x12345678, using only the values provided in the global variable usable_chars
+    # find the values that can be subtracted from the start value to reach the end value.
+    #
+    #   0x00000000
+    # - 0x735f5f25   (%__s)
+    #   ----------
+    #   0x8ca0a0db
+    # - 0x7a6c4a63   (cJlz)
+    #   ----------
+    #   0x12345678
+    #
+    # We will operate on 4 bytes at a time, a dword. These bytes will be stored in a list
+    dword = []
+
+    # Establish our initial structure, loop 4 times with a dummy value, set the depth to 0 and create
+    # an empty list to store our calculated steps to reach the end value
+    for i in range(0,4):
+        dword.append({'start_val':'\x41', 'end_val':'\x00', 'depth':0, 'min_depth':0, 'char_list': [], 'checking':True})
+
+    # Intel is big endian so we need to store the byte backwards
+    dword[3]['value'] = '\x07'
+    dword[2]['value'] = '\x98'
+    dword[1]['value'] = '\x23'
+    dword[0]['value'] = '\x43'
+
+    # We will work one byte at a time, if the addition of two values results in a carry (i.e the values subtracted from
+    # each other require borrowing from the neighboring byte) we need to be aware of that and factor it into our calculation
+    carry = 0
+
+    # The main loop driving the program, one thing we need to consider is that even though we are operating byte by byte
+    # through the dword, we need to cater for the byte with the longest requirement. If one byte can reach our desired value
+    # in two steps, but another requires 3 we need to go back and re-work the byte with the lower requirement to be equal to
+    # the byte with the higher steps. We need to reconsider how the newly generated bytes will be affected by carries as well
+    #
+    # The loop works via checking the depth, we record the byte with the deepest depth in the global var waterline, we run through
+    # this loop continually until all 4 bytes have the equivilent depth
     
-    char_list = []
-    byte1[1] = find_char(byte1[0], "\x00", usable_chars, byte1[2], 0)
-    print("Done", byte1[2], byte1[1])
-    byte2[1] = find_char(byte2[0], "\x00", usable_chars, byte2[2], 0)
-    print("Done", byte2[2], byte2[1])
-    byte3[1] = find_char(byte3[0], "\x00", usable_chars, byte3[2], 0)
-    print("Done", byte3[2], byte3[1])
-    byte4[1] = find_char(byte4[0], "\x00", usable_chars, byte4[2], 0)
-    print("Done", byte4[2], byte4[1])
-    while not check_depth(byte1, byte2, byte3, byte4):
-        print("Unbalanced: %d" % max_depth)
-        input()
-        if byte1[1] < max_depth:
-            byte1[2].clear()
-            byte1[1] = 0
-            byte1[1] = find_char(byte1[0], "\x00", usable_chars, byte1[2], 0)
-        if byte2[1] < max_depth:
-            byte2[1] = 0
-            byte2[2].clear()
-            byte2[1] = find_char(byte2[0], "\x00", usable_chars, byte2[2], 0)
-        if byte3[1] < max_depth:
-            byte3[1] = 0
-            byte3[2].clear()
-            byte3[1] = find_char(byte3[0], "\x00", usable_chars, byte3[2], 0)
-        if byte4[1] < max_depth:
-            byte4[1] = 0
-            byte4[2].clear()
-            byte4[1] = find_char(byte4[0], "\x00", usable_chars, byte4[2], 0)
-            
-    hex_print(byte1, byte2, byte3, byte4)
-    print("Done", byte1[2])
-    print("Done", byte2[2])
-    print("Done", byte3[2])
-    print("Done", byte4[2])
+##    while not check_depth(dword):
+##        # We will work one byte at a time, if the addition of two values results in a carry (i.e the values subtracted from
+##        # each other require borrowing from the neighboring byte) we need to be aware of that and factor it into our calculation
+##        carry = 0
+##        for byte in dword:
+##            byte['char_list'].clear()
+##            byte['depth'] = 0
+##            print("Before carry: %d" % ord(byte['value']))
+##            byte['value'] = chr(ord(byte['value']) + carry)
+##            print("After carry: %d" % ord(byte['value']))
+##            byte['depth'] = find_char(byte['value'], "\x00", byte['char_list'], 0)
+##            carry = get_carry(byte)
+##            print("Done", byte['char_list'], byte['depth'])
+        
+
+
+    # The first run, 
+    for byte in dword:
+        print("Before carry: %d" % ord(byte['value']))
+        byte['value'] = chr(ord(byte['value']) + carry)
+        print("After carry: %d" % ord(byte['value']))
+        byte['depth'] = find_char(byte['value'], "\x00", byte['char_list'], 0)
+        carry = get_carry(byte)
+        print("Done", byte['char_list'], byte['depth'])
+        
+    while not check_depth(dword):
+        print("Unbalanced: %d" % waterline)
+        for byte in dword:
+            if byte['depth'] < waterline:
+                byte['char_list'].clear()
+                byte['depth'] = 0
+                byte['depth'] = find_char(byte['value'], "\x00", byte['char_list'], 0)
+
+    hex_print(dword)
